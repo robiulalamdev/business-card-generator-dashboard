7d33679ac7e644c6172972a9601bef61700a5420
robiulalamdev <robiulalam762400@gmail.com>
2024-02-11 17:00:45 +0600

    ticket form desinged

---
+++ src/components/home/Home.js
@@ -0,0 +1,89 @@
+import { Button } from "@material-tailwind/react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { SpinnerCircularFixed } from "spinners-react";
+
+const Home = () => {
+  const { handleSubmit, register } = useForm();
+  const isLoading = false;
+
+  const handleSave = async (data) => {
+    const options = {
+      data: data,
+    };
+
+    console.log(options);
+    // const result = await toggleGlobal(options);
+    // if (result?.data?.success) {
+    //   toast.success("Data Save Success!");
+    // } else {
+    //   toast.error("Data Save Failed!");
+    // }
+  };
+  return (
+    <main className=" w-full h-screen flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(handleSave)}
+        className="max-w-[500px] w-full mx-auto bg-white h-fit px-3 py-5 rounded-md shadow border"
+      >
+        <div className="mb-4">
+          <span className="block tracking-[0.26px] leading-[16px] text-black text-sm mb-1">
+            Email
+          </span>
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            className="w-full h-10 bg-gray-100 outline-none px-2 rounded text-sm"
+            placeholder="Enter Email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <span className="block tracking-[0.26px] leading-[16px] text-black text-sm mb-1">
+            URL
+          </span>
+          <input
+            {...register("url", { required: true })}
+            type="url"
+            className="w-full h-10 bg-gray-100 outline-none px-2 rounded text-sm"
+            placeholder="Enter URL"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <span className="block tracking-[0.26px] leading-[16px] text-black text-sm mb-1">
+            Code
+          </span>
+          <input
+            {...register("code", { required: true })}
+            type="number"
+            className="w-full h-10 bg-gray-100 outline-none px-2 rounded text-sm"
+            placeholder="Enter Code"
+            required
+          />
+        </div>
+        <div className="mt-5 col-span-4">
+          <Button
+            type="submit"
+            className="flex justify-center items-center gap-2 max-w-[180px] w-full h-10 shadow-none hover:shadow-none rounded bg-primary"
+          >
+            {isLoading && (
+              <SpinnerCircularFixed
+                size={25}
+                thickness={150}
+                speed={450}
+                color="white"
+                secondaryColor="gray"
+              />
+            )}
+            Submit
+          </Button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
+import Home from "@/components/home/Home";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <main className={`${inter.className}`}></main>;
+export default function HomePage() {
+  return (
+    <main className={`${inter.className}`}>
+      <Home />
+    </main>
+  );
 }
