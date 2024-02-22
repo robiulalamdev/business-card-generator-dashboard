1d83bc62159474b45867a16a4700edd159203bad
robiulalamdev <robiulalam762400@gmail.com>
2024-02-22 23:49:10 +0600

    sidebar removed

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -3,7 +3,7 @@ import Sidebar from "@/components/shared/Sidebar";
 export default function MainLayout({ children }) {
   return (
     <main className="flex justify-between items-start h-screen">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <main className="grid grid-cols-1 h-full w-full flex-grow overflow-y-auto bg-white">
         <div className="h-full w-full container mx-auto">{children}</div>
       </main>
